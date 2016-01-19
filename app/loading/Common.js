4aa02758286f581a2a3d90c1fb8744e8bc2777ad
xingyue <qixingyue@126.com>
2016-01-19 11:26:55 +0800

    学习PickerIOS组件的使用

--- app/loading/Common.js
+++ app/loading/Common.js
@@ -8,6 +8,9 @@ var {
 	View
 	,Text
 	,ActivityIndicatorIOS
+	,TextInput
+	,PickerIOS
+	,PickerItemIOS
 } = React;
 
 var LineButton = React.createClass({
@@ -36,9 +39,51 @@ var LoadingView = React.createClass({
 
 });
 
+var ELineFiled = React.createClass({
+
+	render(){
+		return (
+			<View style={{marginTop:10,marginLeft:20}}>
+				<Text>{this.props.label}</Text>	
+				<TextInput 
+					style={[{padding:5,height: 40,borderColor:'gray',borderWidth:1,marginTop:10,marginRight:20},this.props.style]}
+					multiline={this.props.multiline}
+					onChangeText={(text)=>{ this.props.setValueHander && this.props.setValueHander(this.props.name,text);}}
+				/>
+			</View>
+		);
+	}
+
+});
+
+var EPickerField = React.createClass({
+
+	type:"EPickerField"
+
+	,getInitialState(){
+		return {
+			selectedValue:this.props.items[0]
+		};	
+	}
+
+	,render(){
+		return (
+				<PickerIOS
+					selectedValue={this.state.selectedValue}
+					onValueChange={(item) => {this.setState({selectedValue:item}); this.props.setValueHander && this.props.setValueHander(this.props.name,item)}}
+				>
+				{this.props.items.map((item) => {
+					return (<PickerItemIOS key={item} value={item} label={item}></PickerItemIOS>)
+				})}
+				</PickerIOS>
+		);	
+	}
+});
+
 module.exports = {
 
 	LoadingView:LoadingView
 	,LineButton:LineButton
-
+	,ELineFiled:ELineFiled
+	,EPickerField:EPickerField
 };
--- app/loading/addApp.js
+++ app/loading/addApp.js
@@ -8,9 +8,15 @@ var appm = require("./appm");
 
 var {
 	View
-	,Text
-	,TextInput
+	,ScrollView
 } = React;
+
+var {
+	ELineFiled
+	,LoadingView
+	,LineButton
+	,EPickerField
+} = Common;
 
 var addAppItem = {
 	"url":""
@@ -19,22 +25,9 @@ var addAppItem = {
 	,"description":""
 };
 
-var ELineFiled = React.createClass({
-
-	render(){
-		return (
-			<View style={{marginTop:10,marginLeft:20}}>
-				<Text>{this.props.label}</Text>	
-				<TextInput 
-					style={[{padding:5,height: 40,borderColor:'gray',borderWidth:1,marginTop:10,marginRight:20},this.props.style]}
-					multiline={this.props.multiline}
-					onChangeText={(text)=>{addAppItem[this.props.name] = text;}}
-				/>
-			</View>
-		);
-	}
-
-});
+function setValueHander(name,value){
+	addAppItem[name] = value;
+}
 
 
 var AddAppScreen = React.createClass({
@@ -49,24 +42,32 @@ var AddAppScreen = React.createClass({
 		const { navigator } = this.props;
 		if(this.state.netsync){
 			return (
-					<Common.LoadingView loadingText="Add App Item..."/>
+					<LoadingView loadingText="Add App Item..."/>
 			);	
 		} else {
 			return (
 				<View style={styles.container}>
+					<ScrollView>
 					<NavigationBar title={{title:"Add App Item"}} leftButton={{title:"Back",handler:()=>{navigator.pop()}}} />
-					<ELineFiled label="Name:" name="name"/>	
-					<ELineFiled label="ModuleName:" name="moduleName"/>	
-					<ELineFiled label="URL:" name="url"/>	
-					<ELineFiled label="Description:" multiline={true} style={{height:120}} name="description"/>	
-					<Common.LineButton style={{marginTop:20}} onPress={this.addApp} text="Add This App"/>
+					<ELineFiled setValueHander = {setValueHander} label="Name:" name="name"/>	
+					<ELineFiled setValueHander = {setValueHander} label="ModuleName:" name="moduleName"/>	
+					<ELineFiled setValueHander = {setValueHander} label="Description:" multiline={true} style={{height:120}} name="description"/>	
+					<EPickerField 
+						items={["localhost:8081","192.168.1.114:8081","10.217.39.76:8081","10.217.39.251:8081"]}
+						setValueHander={setValueHander}
+						name="url"
+					/>
+					<LineButton style={{marginTop:20}} onPress={this.addApp} text="Add This App"/>
+					</ScrollView>
 				</View>
 			);	
 		}
 	}
 
 	,addApp(){
-		console.log(addAppItem);
+		addAppItem.url = "http://" + addAppItem.url 
+			+ "/app/" + addAppItem.moduleName + "/" 
+			+ addAppItem.moduleName + ".ios.bundle";
 		const { navigator } = this.props;
 		appm.addApp(addAppItem,(res,message)=>{
 			if(res){
