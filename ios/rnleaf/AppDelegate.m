32f68ac073e5a5e4796f7257524d7ed957582de2
xingyue <qixingyue@126.com>
2016-02-26 12:40:42 +0800

    自定义协议显示空界面

--- ios/rnleaf/AppDelegate.m
+++ ios/rnleaf/AppDelegate.m
@@ -11,13 +11,16 @@
 
 #import "RCTRootView.h"
 
-@implementation AppDelegate
+@implementation AppDelegate {
+  NSTimer *timer;
+  NSInteger _count;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
 //  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/app/loading/loading.ios.bundle?platform=ios&dev=true"];
-//  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.112:8081/app/loading/loading.ios.bundle?platform=ios&dev=true"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.114:8081/app/loading/loading.ios.bundle?platform=ios&dev=true"];
   
 // curl -o ./rnleaf/loading.jsbundle "http://localhost:8081/app/loading/loading.ios.bundle"
   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"loading" withExtension:@"jsbundle"];
@@ -26,13 +29,26 @@
                                                       moduleName:@"rnleaf"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
   return YES;
 }
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url  {
+  NSLog(@"%@", [url absoluteString]);
+  
+  // 在 host 等于 item.rnleaf 时，说明一个宝贝详情的 url，
+  // 那么就使用本地的 TBItemDetailViewController 来显示
+  if ([[url host] isEqualToString:@"item.rnleaf"]) {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
+  }
+  return YES;
+}
+
 
 @end
